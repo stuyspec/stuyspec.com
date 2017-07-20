094e5449f870107b8e616fced927b758c42bc111
jkao1 <jkao1@stuy.edu>
2017-07-19 22:59:59 -0400

    built version

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '0.0.37' };
+let VersionInfo = { VERSION : '0.0.39' };
 export default VersionInfo
