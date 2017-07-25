9c2564220ecfb4be8d4afef80f3a48870d191a90
jkao1 <jkao1@stuy.edu>
2017-07-25 00:21:32 -0400

    documented selectors

--- src/js/modules/core/selectors.js
+++ src/js/modules/core/selectors.js
@@ -1,12 +1,22 @@
 import { createSelector } from 'reselect'
 
 const getArticles = state => state.core.entities.articles;
+export const getSections = state => state.core.entities.sections;
+const getRequestedArticleSlug = (state, props) => props.match.params.article_slug;
+const getRequestedSectionSlug = (state, props) => {
+	// split path to get the first part: section name
+	pathParts = props.match.path.split('/');
+	return pathParts[1];
+}
 
+/* Gets articles with only necessary properties
+ */
 export const getHomeArticles = createSelector(
 	[ getArticles ],
 	(articles) => {
 		homeArticles = {}; // for immutability
-		Object.keys(articles).map( (key) => { // for all articles, take a subset of keys
+		// moving all articles in the articles object as a condensed version into homeArticles
+		Object.keys(articles).map( (key) => {
 			article = articles[key];
 			// chooses minimum properties for a link to the article
 			pickedArticle = ( ({ title, slug, section }) => ({ title, slug, section }) ) (article);
@@ -16,56 +26,57 @@ export const getHomeArticles = createSelector(
 	}
 )
 
-// Writes the Filer function for objects.
-// predicate is the function which keys/properties must match
+/* Writes the filter function for objects.
+ * predicate is the function which keys/properties must match
+ */
 Object.filter = (obj, predicate) =>
     Object.keys(obj)
           .filter( key => predicate(obj[key]) )
           .reduce( (res, key) => (res[key] = obj[key], res), {} );
 
-const getRequestedArticleSlug = (state, props) => props.match.params.article_slug;
-
+/* Gets article that matches the requested article slug.
+ */
 export const makeGetArticle = () => {
 	return createSelector(
 		[ getArticles, getRequestedArticleSlug ],
 		(articles, requestedArticleSlug) => {
-			article = Object.filter(articles, article => {
+			nestedArticle = Object.filter(articles, article => {
 				return article.slug === requestedArticleSlug;
 			});
-			// article is currently in { id_key : {} } format
-			for (var key in article) {
-				return article[key];
+			articlesMatched = Object.keys(nestedArticle).length;
+			if (articlesMatched == 1) { // only one article matched
+				for (var key in nestedArticle) {
+					return nestedArticle[key];
+				}
 			}
+			console.log("EXCEPTION: article_slug " + requestedArticleSlug + " matched " + articlesMatched + " articles.");
 		}
 	)
 }
 
-const getRequestedSectionSlug = (state, props) => {
-	pathComponents = props.match.path.split('/');
-	return pathComponents[1];
-}
-export const getSections = state => state.core.entities.sections;
-
+/* Gets section that matches the requested section slug.
+ */
 export const makeGetSection = () => {
 	return createSelector(
 		[ getSections, getRequestedSectionSlug ],
 		(sections, requestedSectionSlug) => {
 			// find the nested section object with a matching slug
-			targetSection = Object.filter(sections, section => {
+			nestedSection = Object.filter(sections, section => {
 				return section.slug === requestedSectionSlug;
 			})
-			sectionsMatched = Object.keys(targetSection).length;
+			sectionsMatched = Object.keys(nestedSection).length;
 			if (sectionsMatched == 1) { // only one section matched
-				var sectionId;
-				for (var key in targetSection) {
-					return targetSection[key];
+				for (var key in nestedSection) {
+					return nestedSection[key];
 				}
 			}
-			console.log("EXCEPTION: section_slug matched " + sectionsMatched + " sections.")
+			console.log("EXCEPTION: section_slug " + requestedSectionSlug + " matched " + sectionsMatched + " sections.");
 		}
 	)
 }
 
+/* Gets all articles within the requested section.
+ */
 export const makeGetSectionArticles = () => {
 	getSection = makeGetSection();
 	return createSelector(
