08c1eedf6eec2fcfa8cd2dd10757baae6befafc9
Jason Kao <jasonkao85@gmail.com>
2017-09-01 09:13:49 -0400

    temporary constants fix

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,5 +1,7 @@
-//export const STUY_SPEC_API_URL = "https://api.stuyspec.xyz";
-export const STUY_SPEC_API_URL = "http://localhost:3000";
+// REMINDER: ALWAYS HTTPS
+
+export const STUY_SPEC_API_URL = "https://api.stuyspec.xyz";
+//export const STUY_SPEC_API_URL = "http://localhost:3000";
 
 export const STUY_SPEC_API_HEADERS = {
   headers: {
