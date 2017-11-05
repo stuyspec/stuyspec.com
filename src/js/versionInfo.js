4a74f36c7f091fb232f7fac4183c86e6e905d739
Jason Kao <jasonkao85@gmail.com>
2017-11-05 16:09:03 -0500

    fixed byline keys

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '0.0.6266' };
+let VersionInfo = { VERSION : '0.0.6267' };
 export default VersionInfo;
