885f02f2ff444e8bc86d7665a720ce96059c8540
Jason Kao <jasonkao85@gmail.com>
2017-11-12 00:50:56 -0500

    tall ad!

---
+++ src/js/modules/advertisements/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+};
+
+const reducer = (state = { ...initialState }, action) => {
+  switch (action.type) {
+    default:
+      break;
+  }
+  return state;
+};
+
+export default reducer;
