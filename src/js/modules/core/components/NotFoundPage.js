50c35e3190d244a485bfd6aa55efd52f0cfe9791
JasonLin43212 <jasonlin43212@gmail.com>
2017-11-19 19:13:50 -0500

    made image for 404 page updated

--- src/js/modules/core/components/NotFoundPage.js
+++ src/js/modules/core/components/NotFoundPage.js
@@ -25,6 +25,7 @@ const styles = {
     },
   },
   img: {
+    height: "75vh",
     width: "50%",
     marginTop: "25px",
   },
@@ -51,7 +52,7 @@ const NotFoundPage = ({ classes }) => {
       <p>
         Please try a modified query, or visit our <Link to="/">home page</Link>.
       </p>
-      <img src="/img/404.jpg" className={classes.img} />
+      <img src="https://i.imgur.com/TqAkThR.png" className={classes.img} />
     </Grid>
   );
 };
