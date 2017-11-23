d7c73177321c187649711cde73e8500dc03f08d9
Nicholas Yang <ny585@nyu.edu>
2017-11-23 15:00:51 -0500

    oops do not change default url

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,15 +1,15 @@
 //export const STUY_SPEC_API_URL = "https://api.stuyspec.xyz";
-export const STUY_SPEC_API_URL = "http://stuyspec-api-prod.us-east-1.elasticbeanstalk.com";//"http://localhost:3000";
+export const STUY_SPEC_API_URL = "http://localhost:3000";
 
 export const STUY_SPEC_API_HEADERS = {
   headers: {
-    "X-Key-Inflection": "camel",
-  },
+    "X-Key-Inflection": "camel"
+  }
 };
 
 export const MEDIA_CREATOR_SLUGS = {
   illustration: "illustrators",
-  photo: "photographers",
+  photo: "photographers"
 };
 
 export const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '0.0.7476' };
+let VersionInfo = { VERSION : '0.0.7480' };
 export default VersionInfo;
