03ced45368108da6cbde2e672a8c7f5bc76975ee
Jason Kao <jasonkao85@gmail.com>
2017-11-24 13:54:02 -0500

    added spec-reefer

--- src/js/modules/articles/components/ArticleBody.js
+++ src/js/modules/articles/components/ArticleBody.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { connect } from "react-redux";
 import injectSheet from "react-jss";
+import { Link } from "react-router-dom";
 import Row from "react-bootstrap/lib/Row";
 import Col from "react-bootstrap/lib/Col";
 
@@ -22,12 +24,41 @@ const styles = {
     "& p:first-child": {
       marginTop: "28px",
     },
-    "& p:first-child::first-letter": {
+    "& > div > p::first-letter": {
       // dropcap
       float: "left",
       fontSize: "58px",
       lineHeight: "43px",
       padding: "7px 6px 0px 3px",
+    },
+    "& > div > p ~ p::first-letter": {
+      float: "none",
+      fontSize: "18px",
+      lineHeight: 1.44,
+      padding: 0,
+    },
+    "& span#article-reefer": {
+      display: "block",
+      fontStyle: "italic",
+      marginTop: "8px",
+      marginBottom: "20px",
+      "& a#reefer-link": {
+        color: "#000",
+        textDecoration: "underline",
+        "&:hover": {
+          color: "#000",
+        },
+        "&:active": {
+          color: "#000",
+        },
+        "&:focus": {
+          color: "#000",
+        },
+      },
+    },
+    "& spec-reefer": {
+      // the original reefer
+      display: "none",
     },
   },
   content: {
@@ -60,24 +91,49 @@ const styles = {
   },
 };
 
-const ArticleBody = ({ classes, content, media }) => {
+const ArticleBody = ({ classes, articles, sections, content, media }) => {
   /*
   const generateFigure = (match, string, offset) => {
     const image = media[parseInt(string)];
-    return (
-      <figure>
-        <img src={image.attachmentUrl} alt={image.title}/>
-        <figcaption>{image.caption}</figcaption>
-      </figure>
-    );
+    if (image) {
+      return `<figure>
+          <img src={image.attachmentUrl} alt={image.title}/>
+          <figcaption>{image.caption}</figcaption>
+        </figure>`;
+    } else {
+      return `<figure>
+          <img src={`mediaId:${string}`} alt={Media not found.}/>
+        </figure>`;
+    }
   };
   // overlap of first spec-img afind = 0; and regular figure and artifletaeuerdmedia
-  specImgPattern = /<spec-img id=(\d)\/>/;
+  specImgPattern = /<spec-img id=(\d)><\/spec-img>/;
   while (specImgPattern.test(content)) {
     content = content.replace(specImgPattern, generateFigure);
   }
   */
   //  featuredMedia = Object.values(articleMedia).find(image => image.isFeatured);
+  const generateArticleReefer = content => {
+    const match = specReeferPattern.exec(content);
+    const article = articles[parseInt(match[1])];
+    if (article) {
+      return (
+        <span id="article-reefer">
+          This article was written in response to &ldquo;
+          <Link
+            id="reefer-link"
+            target="_blank"
+            to={`${sections[article.sectionId].permalink}/${article.slug}`}
+          >
+            {article.title}
+          </Link>
+          &rdquo;, published in Volume {article.volume} Issue{" "}
+          {article.issue}.
+        </span>
+      );
+    }
+  };
+  const specReeferPattern = /<spec-reefer id=(\d*)><\/spec-reefer>/;
   const featuredMedia = media.find(medium => medium.isFeatured);
   return (
     <Row>
@@ -88,6 +144,7 @@ const ArticleBody = ({ classes, content, media }) => {
             isCaption={true}
           />
         )}
+        {specReeferPattern.test(content) && generateArticleReefer(content)}
         <div
           className={classes.innerHTML}
           dangerouslySetInnerHTML={{ __html: content }}
@@ -100,4 +157,9 @@ const ArticleBody = ({ classes, content, media }) => {
   );
 };
 
-export default injectSheet(styles)(ArticleBody);
+const mapStateToProps = state => ({
+  articles: state.articles.articles,
+  sections: state.sections.sections,
+});
+
+export default connect(mapStateToProps)(injectSheet(styles)(ArticleBody));
