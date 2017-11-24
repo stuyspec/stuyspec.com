630bb136b6e827b83011c0f5079a22a3f99a42ff
Jason Kao <jasonkao85@gmail.com>
2017-11-24 18:50:54 -0500

    search articles action

--- src/js/modules/articles/actions.js
+++ src/js/modules/articles/actions.js
@@ -3,6 +3,27 @@ import { STUY_SPEC_API_URL, STUY_SPEC_API_HEADERS } from "../../constants";
 import { validateKey } from "../../utils";
 import * as t from "./actionTypes";
 
+export const searchArticles = query => {
+  return dispatch => {
+    dispatch({ type: t.SEARCH_ARTICLES_PENDING });
+    axios
+      .get(`${STUY_SPEC_API_URL}/articles?query=${encodeURI(query)}`, STUY_SPEC_API_HEADERS)
+      .then(response => {
+        dispatch({
+          type: t.SEARCH_ARTICLES_FULFILLED,
+          payload: response.data,
+        });
+      })
+      .catch(err => {
+        dispatch({
+          type: t.SEARCH_ARTICLES_REJECTED,
+          payload: err,
+        });
+      });
+  };
+
+}
+
 export const fetchArticles = () => {
   return dispatch => {
     dispatch({ type: t.FETCH_ARTICLES_PENDING });
