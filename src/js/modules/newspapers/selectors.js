4a32f13a38d28361b72a74d435e7e03d9e1f80b5
Jason Kao <jasonkao85@gmail.com>
2017-11-24 22:14:22 -0500

    newspaper selector

---
+++ src/js/modules/newspapers/selectors.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { createSelector } from "reselect";
+
+export const getNewspaperFromSlug = (state, props) => {
+  // go into Reactdevtools in chrome, and try to find a component with props match. match.params.volume and match.params.issue
+};
