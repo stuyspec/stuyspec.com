bcffa2e5b6803517757cdd4440db3009e16cfe42
Jason Kao <jasonkao85@gmail.com>
2017-11-25 09:37:45 -0500

    lightbox

---
+++ src/js/modules/core/components/Lightbox.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import injectJSS from "react-jss";
+
+import { openLightbox, closeLightbox } from "../actions";
+
+const styles = {
+  Lightbox: {
+    background: "#000",
+    display: "flex",
+    "-webkit-box-align": "center",
+    "-ms-flex-align": "center",
+    alignItems: "center",
+    display: "-webkit-box",
+    display: "-ms-flexbox",
+    display: "flex",
+    height: "100%",
+    "-webkit-box-pack": "center",
+    "-ms-flex-pack": "center",
+    justifyContent: "center",
+    left: 0,
+    padding: "44px 0 0",
+    position: "fixed",
+    top: 0,
+    width: "100%!important",
+    zIndex: 10000,
+  },
+  closeButton: {
+    background: "transparent",
+    border: 0,
+    cursor: "pointer",
+    height: "24px",
+    left: "auto",
+    position: "absolute",
+    right: "35px",
+    top: "35px",
+    width: "24px",
+  },
+};
+
+const Lightbox = ({
+  classes,
+  children,
+  lightboxIsOpen,
+  openLightbox,
+  closeLightbox,
+}) => {
+  if (lightboxIsOpen) {
+    return (
+      <div className={classes.Lightbox}>
+        <button onClick={closeLightbox} className={classes.closeButton} />
+      </div>
+    );
+  } else {
+    return <span className="lightbox-in-hiding" />;
+  }
+};
+
+const mapStateToProps = state => ({
+  lightboxIsOpen: state.core.lightboxIsOpen,
+});
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ openLightbox, closeLightbox }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+  injectJSS(styles)(Lightbox),
+);
