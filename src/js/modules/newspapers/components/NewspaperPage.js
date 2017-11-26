ebb8956ab8802bcb9339a45d846c18c2b2371d75
Jason Kao <jasonkao85@gmail.com>
2017-11-26 18:35:58 -0500

    added newspaper TODO

--- src/js/modules/newspapers/components/NewspaperPage.js
+++ src/js/modules/newspapers/components/NewspaperPage.js
@@ -1,1 +1,2 @@
-// make a component, NewspaperPage, that takes volume & issue props and does what i drew on the whiteboard
+// TODO: make a component, NewspaperPage, that takes volume & issue props and does what i drew on the whiteboard
+// @ccai1
