7076f315b8dc1fd56c0551e3f79a60590c832a5a
Jason Kao <jasonkao85@gmail.com>
2017-11-26 20:01:00 -0500

    made error msges more visible

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '0.0.8035' };
+let VersionInfo = { VERSION : '0.0.8036' };
 export default VersionInfo;
