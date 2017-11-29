2db1c8635cf83c278deccfed678b13d629eb00be
Jason Kao <jasonkao85@gmail.com>
2017-11-29 14:57:00 -0500

    dateline

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '0.0.8090' };
+let VersionInfo = { VERSION : '0.0.8111' };
 export default VersionInfo;
