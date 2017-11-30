fa525bb8c2d4431163d12f784e86733ad0ce9ebc
Jason Kao <jasonkao85@gmail.com>
2017-11-29 19:36:45 -0500

    HOTFIX: API URL HERE

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,9 +1,6 @@
 // Will get replaced by gulpfile with relevant environment url
 export const STUY_SPEC_API_URL = "__API_URL_HERE__";
 
-export const STUY_SPEC_API_URL = "https://api.stuyspec.xyz";
-//export const STUY_SPEC_API_URL = "http://localhost:3000";
-
 export const STUY_SPEC_API_HEADERS = {
   headers: {
     "X-Key-Inflection": "camel"
