51e968545a3e85a22319f468924e89a7ce88e099
Jason Kao <jasonkao85@gmail.com>
2017-11-29 19:32:54 -0500

    masthea styling

--- src/js/modules/core/components/Masthead.js
+++ src/js/modules/core/components/Masthead.js
@@ -77,14 +77,46 @@ const styles = {
     fontSize: "14px",
     fontWeight: "bold",
     textAlign: "center",
-    color: "#000000",
+    color: "#000",
   },
   subscribeText: {
     fontFamily: "Circular Std",
     fontSize: "15px",
     fontWeight: "bold",
     textAlign: "center",
-    color: "#ffffff",
+    color: "#fff",
+  },
+  signedInNav: {
+    top: "5px",
+    display: "flex",
+    position: "relative",
+  },
+  myAccount: {
+    display: "inline",
+    fontFamily: "Circular Std",
+    fontSize: "14px",
+    fontWeight: "bold",
+    color: "#000",
+    transitionDuration: ".3s",
+    "&:hover, &:active, &:focus": {
+      color: "#000",
+    },
+  },
+  searchLink: {
+    color: "#000",
+    position: "relative",
+    height: "17px",
+    width: "17px",
+    marginLeft: "15px",
+    top: "1px",
+    "& svg": {
+      height: "100%",
+      width: "100%",
+    },
+    "&:hover, &:active, &:focus": {
+      color: "#000",
+      textDecoration: "none",
+    },
   },
   subscribeTo: {
     position: "relative",
@@ -192,20 +224,31 @@ const Masthead = ({
       <Link className={classes.theSpectatorLogo} to="/">
         The Spectator
       </Link>
-      {!session && (
-        <table className={classes.positioning}>
-          <tbody>
-            <tr>
-              <th id="problematic-subscribe-button-container-in-moz">
-                <StyledNavButton
-                  onClick={openSubscriptionModal}
-                  type="Subscribe"
-                >
-                  <span className={classes.subscribeText}>Subscribe</span>
-                  <br />
-                  <span className={classes.subscribeTo}>to our newsletter</span>
-                </StyledNavButton>
-              </th>
+      <table className={classes.positioning}>
+        <tbody>
+          <tr>
+            <th id="problematic-subscribe-button-container-in-moz">
+            {session ? (
+              <div className={classes.signedInNav}>
+                <Link to="/myaccount/profile" className={classes.myAccount}>
+                  My Account
+                </Link>
+                <Link to="/search" className={classes.searchLink}>
+                  <Search className={classes.searchButton} />
+                </Link>
+              </div>
+            ) : (
+              <StyledNavButton
+                onClick={openSubscriptionModal}
+                type="Subscribe"
+              >
+                <span className={classes.subscribeText}>Subscribe</span>
+                <br />
+                <span className={classes.subscribeTo}>to our newsletter</span>
+              </StyledNavButton>
+            )}
+            </th>
+            {!session && (
               <th>
                 <div className={classes.userTools}>
                   <Link to="/myaccount/profile">
@@ -215,10 +258,10 @@ const Masthead = ({
                   </Link>
                 </div>
               </th>
-            </tr>
-          </tbody>
-        </table>
-      )}
+            )}
+          </tr>
+        </tbody>
+      </table>
       <ul className={classes.sectionLinksNav}>
         {Object.values(sections).map(section => {
           if (!unwantedSectionNames.includes(section.name)) {
@@ -246,12 +289,12 @@ const Masthead = ({
             The Paper
           </Link>
         </li>
-      */}
         <li key={-3} className={classes.sectionListItem}>
           <Link to="/search" className={classes.sectionLink}>
             <Search className={classes.navSearchButton} />
           </Link>
         </li>
+      */}
       </ul>
     </div>
   );
