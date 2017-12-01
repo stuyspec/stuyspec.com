c89ca0850f144856b7d490995c07c52a4fc6df60
Jason Lin <jasonlin43212@gmail.com>
2017-11-30 21:22:06 -0500

    .

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '0.0.8436' };
+let VersionInfo = { VERSION : '0.0.8437' };
 export default VersionInfo;
