f20f3c31708e42000f9fc6d43bbe5571832d9df7
Jason Kao <jasonkao85@gmail.com>
2017-12-01 19:33:58 -0500

    fixed constants

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '0.0.8472' };
+let VersionInfo = { VERSION : '0.0.8473' };
 export default VersionInfo;
