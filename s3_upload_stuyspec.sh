036f12276361256d7ad50bd8a3c9d6ffbf2e89d3
Jason Kao <jasonkao85@gmail.com>
2017-12-04 12:59:16 -0500

    temp no invalidation-completed

--- s3_upload_stuyspec.sh
+++ s3_upload_stuyspec.sh
@@ -1,5 +1,3 @@
 sudo gulp build
 aws s3 cp dist/prod/build.js s3://stuyspec
-aws cloudfront create-invalidation --distribution-id $STUYSPEC_CLOUDFRONT_DISTRIBUTION_ID --paths /build.js |
-	jq -r '.Invalidation.Id' |
-	aws cloudfront invalidation-completed --distribution-id $STUYSPEC_CLOUDFRONT_DISTRIBUTION_ID -id read -r
+aws cloudfront create-invalidation --distribution-id $STUYSPEC_CLOUDFRONT_DISTRIBUTION_ID --paths /build.js
