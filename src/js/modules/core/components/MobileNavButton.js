19c4314d40b8ad58d4701a277b9c74495fc71048
Jason Kao <jasonkao85@gmail.com>
2018-01-04 14:20:43 -0500

    mobilenavbutton

---
+++ src/js/modules/core/components/MobileNavButton.js
@@ -0,0 +1,42 @@
+import React from "react";
+import injectSheet from "react-jss";
+
+const navButtonStyles = {
+  MobileNavButton: {
+    background: "none",
+    borderWidth: 0,
+    display: "none",
+    margin: 0,
+    padding: 0,
+    "&:hover": {
+      cursor: "pointer",
+    },
+  },
+  buttonText: {
+    color: "#000",
+    fontFamily: "Circular Std",
+    fontSize: "12px",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+  },
+  icon: {
+    display: "inline",
+    marginRight: "4px",
+  },
+  "@media (max-width: 768px)": {
+    MobileNavButton: {
+      display: "block",
+    },
+  },
+};
+
+const MobileNavButton = ({ classes, children, label, onClick }) => {
+  return (
+    <button className={classes.MobileNavButton} onClick={onClick}>
+      <div className={classes.icon}>{children}</div>
+      <span className={classes.buttonText}>{label}</span>
+    </button>
+  );
+};
+
+export default injectSheet(navButtonStyles)(MobileNavButton);
