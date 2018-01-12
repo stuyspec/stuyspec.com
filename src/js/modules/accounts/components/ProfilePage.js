721ebb621ee07be5390e32d43f627c7d9cf807d4
Jason Kao <jasonkao85@gmail.com>
2018-01-12 13:09:27 -0500

    profile page -> apollo

--- src/js/modules/accounts/components/ProfilePage.js
+++ src/js/modules/accounts/components/ProfilePage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+import { graphql } from "react-apollo";
+import humps from "humps";
 import { Link } from "react-router-dom";
 import injectSheet from "react-jss";
 import { Grid, Row, Col, Table } from "react-bootstrap/lib";
@@ -8,6 +10,7 @@ import { Helmet } from "react-helmet";
 
 import { SignOutForm } from "./forms";
 import { signOut } from "../actions";
+import { UserByUIDQuery } from "../../../queries";
 
 const styles = {
   pageTitle: {
@@ -41,69 +44,75 @@ const styles = {
   },
 };
 
-const ProfilePage = ({ classes, signOut, session, sessionUser }) => {
-  if (sessionUser) {
-    return (
-      <Grid fluid>
-        <Helmet titleTemplate="%s | The Stuyvesant Spectator">
-          <title>Profile</title>
-          <meta />
-        </Helmet>
-        <Row>
-          <Col
-            xs={12}
-            sm={6}
-            smOffset={3}
-            md={6}
-            mdOffset={3}
-            lg={6}
-            lgOffset={3}
-          >
-            <p className={classes.pageTitle}>
-              Welcome, {sessionUser.firstName}.
-            </p>
-            <Link
-              to={"/myaccount/profile/edit"}
-              className={classes.editRedirect}
-            >
-              Edit Profile
-            </Link>
-            <div className={classes.dataTable}>
-              <Table responsive>
-                <tbody>
-                  <tr>
-                    <td>First Name</td>
-                    <td>{sessionUser.firstName}</td>
-                  </tr>
-                  <tr>
-                    <td>Last Name</td>
-                    <td>{sessionUser.lastName}</td>
-                  </tr>
-                  <tr>
-                    <td>E-mail Address</td>
-                    <td>{sessionUser.email}</td>
-                  </tr>
-                </tbody>
-              </Table>
-            </div>
-            {/* We keep the SignOutForm to display the success message */}
-            <SignOutForm onSubmit={() => signOut(session)} />
-          </Col>
-        </Row>
-      </Grid>
-    );
+const ProfilePage = ({ classes, signOut, session, data }) => {
+  if (data.loading) {
+    return null;
   }
+  data = humps.camelizeKeys(data);
+  const currentUser = data.userByUID;
+
+  return (
+    <Grid fluid>
+      <Helmet titleTemplate="%s | The Stuyvesant Spectator">
+        <title>Profile</title>
+      </Helmet>
+      <Row>
+        <Col
+          xs={12}
+          sm={6}
+          smOffset={3}
+          md={6}
+          mdOffset={3}
+          lg={6}
+          lgOffset={3}
+        >
+          <p className={classes.pageTitle}>
+            Welcome, {currentUser.firstName}.
+          </p>
+          <Link
+            to={"/myaccount/profile/edit"}
+            className={classes.editRedirect}
+          >
+            Edit Profile
+          </Link>
+          <div className={classes.dataTable}>
+            <Table responsive>
+              <tbody>
+                <tr>
+                  <td>First Name</td>
+                  <td>{currentUser.firstName}</td>
+                </tr>
+                <tr>
+                  <td>Last Name</td>
+                  <td>{currentUser.lastName}</td>
+                </tr>
+                <tr>
+                  <td>E-mail Address</td>
+                  <td>{currentUser.email}</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+          <SignOutForm onSubmit={() => signOut(session)} />
+        </Col>
+      </Row>
+    </Grid>
+  );
 };
 
 const mapStateToProps = state => ({
   session: state.accounts.session,
-  sessionUser: getCurrentUser(state),
 });
 
 const mapDispatchToProps = dispatch => {
   return bindActionCreators({ signOut }, dispatch);
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(
-  injectSheet(styles)(ProfilePage),
-);
+export default compose(
+  graphql(UserByUIDQuery, {
+    // no skip is necessary because !session redirects to /myaccount from RoutingApp
+    options: ({ session }) => ({ variables: { uid: session.uid } }),
+  }),
+  connect(mapStateToProps, mapDispatchToProps),
+  injectSheet(styles),
+)(ProfilePage);
