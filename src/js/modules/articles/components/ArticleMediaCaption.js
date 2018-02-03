66b1c1f059a45a9f4334dd618768c24fa0a516e1
Jason Kao <jasonkao85@gmail.com>
2018-02-03 10:44:25 -0500

    prettier media caption

--- src/js/modules/articles/components/ArticleMediaCaption.js
+++ src/js/modules/articles/components/ArticleMediaCaption.js
@@ -20,11 +20,15 @@ const styles = {
 
 const ArticleMediaCaption = ({ classes, image }) => {
   const { user } = image;
+  let { caption } = image;
+  if (caption && caption.substr(caption.length - 1) !== ".") {
+    caption += ".";
+  }
   return (
     <figcaption className={classes.caption}>
       <span>
         {/* Render caption if caption is not null and is not empty string. */}
-        {image.caption && image.caption + " "}
+        {caption && caption + " "}
       </span>
       <Link
         className={classes.creditLine}
