a3b88d946d4c5e89d9b101de349cb22ef8ed9cf1
Jason Kao <jasonkao85@gmail.com>
2018-02-07 19:58:48 -0500

    prettier

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,44 +1,11 @@
-import { SPEC_REFERENCE_PATTERN, SPEC_IMG_CAROUSEL_PATTERN } from './constants';
+import { SPEC_REFERENCE_PATTERN } from './constants';
 
 export const capitalizeWord = string => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
-export const shortenSummary = article => {
-  if (article.content === undefined || article.summary === undefined) {
-    console.error(
-      `Article: ${article}. Missing content or summary field in shortenSummary.`,
-    );
-    return null;
-  }
-  let articleSummary = article.summary.split(' ');
-  if (articleSummary.length > 25) {
-    articleSummary = articleSummary.slice(0, 24).join(' ') + '...';
-  } else if (article.summary === '') {
-    articleSummary = article.content
-      .replace(SPEC_REFERENCE_PATTERN, '')
-      .replace(SPEC_IMG_CAROUSEL_PATTERN, '')
-      .replace(/<p>|<\/p>|<br>|<br\/>|<b>/g, '')
-      .replace(/<h4>|<\/h4>/g, ' ')
-      .replace(/<i>|<\/i>/g, ' ')
-      .replace(/<\/b>/g, ' ');
-    return (
-      articleSummary
-        .split(' ')
-        .slice(0, 24)
-        .join(' ') + '...'
-    );
-  } else {
-    articleSummary = articleSummary.join(' ');
-  }
-  return articleSummary;
-};
-
-/**
- * Writes the filter function for objects.
- * @param obj
- * @param {function} predicate is the function which keys/properties must match.
- */
+// The filter function, but for objects. The predicate is the filtering
+// function.
 export const objectFilter = (obj, predicate) =>
   Object.keys(obj)
     .filter(key => predicate(obj[key]))
--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '1.0.151' };
+let VersionInfo = { VERSION: '1.0.159' };
 export default VersionInfo;
