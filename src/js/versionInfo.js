ca2ac96f66471195e9cb6831163e0a6702c6a10b
Jason Lin <jasonlin43212@gmail.com>
2018-02-17 17:27:22 -0500

    chagned the background of the issuu

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '0.0.8869' };
+let VersionInfo = { VERSION : '0.0.8871' };
 export default VersionInfo;
