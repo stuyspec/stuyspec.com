1ab562fa73f32dc5826df28882ed11a745b4589e
Jason Kao <jasonkao85@gmail.com>
2018-02-22 16:17:54 -0500

    bumped version info to 2.0.0

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '1.0.505' };
+let VersionInfo = { VERSION : '2.0.0' };
 export default VersionInfo;
