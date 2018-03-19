83a7edcb9ed850e631b8ea33169fe601a500799e
Darius Jankauskas <d5926j@gmail.com>
2018-03-18 23:55:11 -0400

    essage=production data added

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '2.0.526' };
+let VersionInfo = { VERSION : '2.0.529' };
 export default VersionInfo;
