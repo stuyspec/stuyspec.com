647b40ff114e15509d693da08ecc2f3b699a93b2
Jason Kao <jasonkao85@gmail.com>
2018-08-27 12:13:30 -0400

    ok then

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '2.0.694' };
+let VersionInfo = { VERSION : '2.0.696' };
 export default VersionInfo;
