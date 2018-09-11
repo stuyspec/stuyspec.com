24d4090d27101744312be9e684206186ae7e83be
Jason Kao <jasonkao85@gmail.com>
2018-09-11 15:29:39 -0400

    ok then 2

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '2.0.696' };
+let VersionInfo = { VERSION : '2.0.697' };
 export default VersionInfo;
