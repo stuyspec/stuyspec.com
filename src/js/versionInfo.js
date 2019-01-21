3342246edc915a1058cfe7302f4ea4edc6a50963
Darius Jankauskas <d5926j@gmail.com>
2019-01-21 18:43:39 -0500

    refactor to use create-react-app

--- src/js/versionInfo.js
+++
@@ -1,8 +0,0 @@
-/**
- * Warning: if editing this file, be sure to keep the VERSION variable set to 'X.X.X'
- *           without modifying formatting for automation scripts
- * @type {{VERSION: string}}
- */
-
-let VersionInfo = { VERSION : '2.0.960' };
-export default VersionInfo;
