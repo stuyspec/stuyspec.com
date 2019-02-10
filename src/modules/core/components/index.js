6a17af386b6ac719d02e8b5ca5d2c1b7b3a80341
Darius Jankauskas <d5926j@gmail.com>
2019-02-09 22:22:37 -0500

    updated import

--- src/modules/core/components/index.js
+++ src/modules/core/components/index.js
@@ -3,4 +3,4 @@ export { default as PageLayout } from "./PageLayout";
 export { default as NotFoundPage } from "./NotFoundPage";
 export { default as DataErrorPage } from "./DataErrorPage";
 export { default as Lightbox } from "./Lightbox";
-export { default as ClassifiedsRedirect } from "./ClassifiedsRedirect";
+export { ClassifiedsRedirect } from "./ClassifiedsRedirect";
