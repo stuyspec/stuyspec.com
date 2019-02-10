b651182da94c3c414bbf301cae060c98a7a17ce8
Darius Jankauskas <d5926j@gmail.com>
2019-02-09 22:21:50 -0500

    Readded missing files

---
+++ src/modules/core/components/ClassifiedsRedirect.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const ClassifiedsRedirect = () => {
+    window.location = 'https://goo.gl/forms/bltSaUMOwVW5TpN02';
+    return (
+        <p>
+            If you're not automatically redirected, please <a href="https://goo.gl/forms/bltSaUMOwVW5TpN02">click here.</a>
+        </p>
+    );
+}
