360c6c8cba93085ecedaecfed5b02925d130cc95
Darius Jankauskas <d5926j@gmail.com>
2019-04-21 00:16:08 -0400

    Converted to ts.

---
+++ src/constants.ts
@@ -0,0 +1,22 @@
+// Will get replaced by CRA with relevant environment url
+export const STUY_SPEC_API_URL = process.env.REACT_APP_API_URL;
+
+// TODO: a Newspaper model in API that contains an issuu_config
+export const ISSUU_CONFIG = 61744139;
+
+export const STUY_SPEC_API_HEADERS = {
+  headers: {
+    "X-Key-Inflection": "camel",
+  },
+};
+
+export const PROFILE_SLUGS: { [index: string] : string} = {
+  illustration: "illustrators",
+  photo: "photographers",
+  illustrators: "illustration",
+  photographers: "photo",
+};
+
+export const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+export const SPEC_REFERENCE_PATTERN = /<spec-reference id=(\d*)><\/spec-reference>/;
+export const SPEC_IMG_CAROUSEL_PATTERN = /<spec-img-carousel><\/spec-img-carousel>/;
