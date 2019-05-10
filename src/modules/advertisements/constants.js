3c03bf4ab8ed2eb2368aa52a934f118b1dc9595c
Darius Jankauskas <d5926j@gmail.com>
2019-05-10 18:57:02 -0400

    Added ad redirects for analytics.

--- src/modules/advertisements/constants.js
+++ src/modules/advertisements/constants.js
@@ -4,3 +4,5 @@ export default {
 
 //PUBLIC_URL replaced by path to public folder at build time
 export const pathToAds = process.env.PUBLIC_URL + "/img/ads/";
+
+export const adRedirects = [['compass-to-college', 'http://www.compasstocollege.org']];
