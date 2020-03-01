44c694ed9ea846c7d88c0448ae61bb03cb2fa8d3
cor100 <cynthiacy.kjy@gmail.com>
2020-03-01 16:41:35 -0500

    removed extra tag

--- src/modules/descriptions/reducer.js
+++ src/modules/descriptions/reducer.js
@@ -328,10 +328,9 @@ Kerry Garfinkel
     title: "Contact",
     slug: "contact",
     content: `<p style="text-align: center">
-  For website related questions: Contact <a href="mailto:web@stuyspec.com">web@stuyspec.com</a>.
-</p>
-<p style = "text-align: center">
-   For all other questions: Contact <a href="mailto:erintalia20@gmail.com">erintalia20@gmail.com</a>.
+  For website related questions: Contact <a href="mailto:web@stuyspec.com">web@stuyspec.com</a>. <br/>
+  For all other questions: Contact <a href="mailto:erintalia20@gmail.com">erintalia20@gmail.com</a>.
+
 </p>
 `,
   },
