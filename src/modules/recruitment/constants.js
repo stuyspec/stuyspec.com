27c608830b2d2a20c967fb15a17929f0596cb705
Andrey Sokolov <andrey.sokolov123@gmail.com>
2021-03-16 22:05:51 -0400

    constants update

--- src/modules/recruitment/constants.js
+++ src/modules/recruitment/constants.js
@@ -2,7 +2,7 @@ export const SECTIONS = [
   {
     name: "Opinions",
     imageLink: null,
-    description: `The Opinions section lets writers share their views on issues relevant to the country, Stuyvesant high school, and their personal lives. Writers across all grades give their opinions on subjects ranging from politics to economics and from technology to culture and share their personal stories. We try to consistently create engaging, substantive, and relevant content to provide students with a closer look at the world and the Stuyvesant community.`,
+    description: `The Opinions section lets writers share their views on issues relevant within the country, Stuyvesant, and their personal lives. Writers across all grades give their opinions on subjects ranging from politics and economics to technology and culture and share their personal stories. We try to consistently create engaging, substantive, and relevant content to provide students with a closer look at the world and the Stuyvesant community.`,
     quotes: [],
     link:
       "",
@@ -10,8 +10,7 @@ export const SECTIONS = [
   {
     name: "Arts and Entertainment",
     imageLink: null,
-    description: `As the only department with more than three article categories, Arts and Entertainment is notable for its versatility and creative freedom. We review recent cultural topics such as art, fashion, film, food, literature, music and more! A&E writers are also free to experiment with the thinkpiece category, where one can explore cultural phenomena or trends, gender standards or even race with a cultural standpoint. Whether you are an aspiring music enthusiast or cultural connoisseur, everyone can find (or create!) their own niche in A&E. 
-    `,
+    description: `As the only department with more than three article categories, Arts and Entertainment is notable for its versatility and creative freedom. We review recent cultural topics such as art, fashion, film, food, literature, music, and more! A&E writers are also free to experiment with the thinkpiece category, where one can explore cultural phenomena or trends, gender standards, or race from a cultural standpoint. Whether you are an aspiring music enthusiast or cultural connoisseur, everyone can find (or create!) their own niche in A&E.`,
     quotes: [],
     link:
       "",
@@ -19,7 +18,7 @@ export const SECTIONS = [
   {
     name: "Features",
     imageLink: null,
-    description: `Features capture the spirit and mood of the school. At times, Features covers Stuyvesant’s evergreen topics, e.g. what is it about PE uniforms that gets everybody so hot and bothered? Other times, writers might profile a teacher. Still other times, in the “Voices” section, writers share creative narratives about experiences they have had. There is much to be found in Features, so come hither!`,
+    description: `Features capture the spirit and mood of the school. At times, Features covers Stuyvesant’s evergreen topics, e.g. what is it about PE uniforms that gets everybody so hot and bothered? Other times, writers might profile a teacher. Other times, writers share creative narratives about experiences they have had in the “Voices” section. There is much to be found in Features, so come hither!`,
     quotes: [],
     link: 
       "",
@@ -38,7 +37,7 @@ export const SECTIONS = [
   {
     name: "Sports",
     imageLink: null,
-    description: `The Sports department covers all things sports, whether that be PSAL sports played at Stuyvesant or professional sports. Stuyvesant articles range from championship articles to athlete and coach interviews, to opinions. Writers also cover virtually any topic in the larger sports world, from debates on LeBron vs. MJ to articles on Youtube boxing and LaVar Ball. If that’s not enough to convince you to apply, we have an exclusive fantasy football league ($ rewards).`,
+    description: `The Sports department covers all things sports, whether it be PSAL sports at Stuyvesant or professional sports. Articles pertaining to Stuyvesant sports range from championship articles and season updates to athlete and coach interviews and opinions. Writers also cover virtually any topic in the larger sports world, from debates on LeBron vs. MJ and college basketball to YouTube boxing and the Champions League. If that’s not enough to convince you to apply, we have an exclusive fantasy football league (with rewards).`,
     quotes: [],
     link:
       "",
@@ -46,8 +45,7 @@ export const SECTIONS = [
   {
     name: "News",
     imageLink: null,
-    description: `The News Department covers school events, changes in administration and student organizations, school and student government policies, and out-of-school breaking news. Besides the traditional News article, we also write mini-articles, graphs & surveys, and spreads. If you are interested in knowing Stuyvesant news before it is released, informing the student body, meeting students and teachers and administrators, or seeing your work on the first page, we hope you apply to our department!
-    `,
+    description: `The News Department covers school events, administrative changes, student organizations, school and student government policies, and out-of-school news. Besides traditional News articles, we also write mini-articles, graphs & surveys, and spreads. If you are interested in knowing Stuyvesant news before it is released, informing the student body, meeting students, teachers, and administrators, or seeing your work on the front page, apply to our department!`,
     quotes: [],
     link:
       "",
@@ -55,7 +53,7 @@ export const SECTIONS = [
   {
     name: "Copy",
     imageLink: null,
-    description: `Members of the Copy Department ensures that The Spectator produces clear, grammaticaly correct, and accurate articles. Did you notice both mistakes in the last sentence? If you did, you should join the Copy Department! Members gain early access to articles and sharpen their grammar skills while editing and fact-checking departments of their choice. We are committed to catching grammatical and factual mistakes in articles with attention to detail, and we look for members who want to contribute to maintaining The Spectator’s high-quality content.`,
+    description: `Members of the Copy Department ensures that The Spectator produces clear, grammatically correct, and accurate articles. Did you notice both mistakes in the last sentence? If you did, you should join the Copy Department! Members gain early access to articles and sharpen their grammar skills while editing and fact-checking departments of their choice. We are committed to catching grammatical and factual mistakes in articles with attention to detail, and we look for members who want to contribute to maintaining The Spectator’s high-quality content.`,
     quotes: [],
     link:
       "",
@@ -63,16 +61,15 @@ export const SECTIONS = [
   {
     name: "Web",
     imageLink: null,
-    description: `The Web department is responsible for designing and updating The Spectator's website, www.stuyspec.com. Web teaches how to develop and maintain a server using Ruby on Rails and a website using Javascript and React. These real-world skills are a great resume builder and provide invaluable experience in working together as a team. This is the perfect department for both computer programming enthusiasts and novices who want to help The Spectator continue to storm the frontier of online journalism.`,
+    description: `The Web department is responsible for designing and updating The Spectator's website, www.stuyspec.com. It also attempts to facilitate the learning of developing and maintaining a web application based on Ruby on Rails and React. These real-world skills are a great resume builder and provide invaluable experience in working together as a team. This is the perfect department for both computer programming enthusiasts and novices who want to help The Spectator continue to storm the frontier of online journalism.`,
     quotes: [],
     link:
-      "https://forms.gle/km8AhjzxDDEhEQbF8",
+      "",
   },
   {
     name: "Layout",
     imageLink: null,
-    description: `A good publication should interest readers before they even pick it up and read it; the Layout department does just that! We are the last department that works with the newspaper and are responsible for formatting all of the articles, photos, art, etc. We make sure that the paper is aesthetically appealing and easy to read. After all, no one wants to read ugly and confusing blocks of text. With creativity and a certain magic called the InDesign, we present to you what you know as The Spectator. As a staff member, you will be taught how to use InDesign and asked to critique the formatting of each issue as it is released.
-    `,
+    description: `A good publication should interest readers before they even pick it up and read it; the Layout department does just that! We are responsible for formatting all of the articles, photos, art, and other media and make sure that the paper is aesthetically appealing and easy to read. After all, no one wants to read ugly and confusing blocks of text. With creativity and a certain magic called InDesign, we present to you what you know as The Spectator. As a staff member, you will be taught how to use InDesign and asked to critique the formatting of each issue as it is released.`,
     quotes: [],
     link:
       "",
@@ -80,8 +77,7 @@ export const SECTIONS = [
   {
     name: "Photo",
     imageLink: null,
-    description: `Photo is dedicated to providing interesting and dynamic photographs to support the articles in The Spectator and spearhead photo essay projects. Typical assignments include portraits of students and teachers, photos at school events, and shots of fun things around the city as well. This department is a good fit for anyone interested in photography, whether you’re just starting out, or you’re an expert looking for a place to practice and refine your skills.
-    `,
+    description: `Photo is dedicated to providing interesting and dynamic photographs to support the articles in The Spectator and spearhead photo essay projects. Typical assignments include portraits of students and teachers, photos at school events, and shots of fun things around the city. This department is a good fit for anyone interested in photography, whether you’re just starting out, or you’re an expert looking for a place to practice and refine your skills.`,
     quotes: [],
     link:
       "",
@@ -89,8 +85,7 @@ export const SECTIONS = [
   {
     name: "Business",
     imageLink: null,
-    description: `The Business department is the backbone of The Spectator. Due to the efforts of this department, The Spectator is able to print all of its issues while still remaining independent. We negotiate and secure deals with businesses all over New York, as well as handle the advertisements on the website and in the paper. Members learn skills that can be applied to real life, including talking in formal settings, learning how to negotiate and compromise, and learning how to manage money. 
-    `,
+    description: `The Business department is the backbone of The Spectator. Due to the efforts of this department, The Spectator is able to print all of its issues while still remaining independent. We negotiate and secure deals with businesses all over New York as well as handle the advertisements on the website and in the paper. Members learn skills that can be applied to real life, including talking in formal settings, learning how to negotiate and compromise, and understanding how to manage money.`,
     quotes: [],
     link:
       "",
@@ -98,8 +93,7 @@ export const SECTIONS = [
   {
     name: "Art",
     imageLink: null,
-    description: `It is often difficult to read through dense blocks of text without something to rest your eyes on. In the Art Department, we strive to embellish the newspaper with our unique illustrations, each perfectly suited for its article. In a school where artistic abilities are not seen as priority, you can easily find your niche as an artist for the Spectator, where your art will be displayed for all of its readers to admire. The limits of your creativity have no bounds; you can explore and develop your art skills by taking on cool and unique requests, from a portrait of Taylor Swift (A&E) to an oily stork (Humor), as well as the occasional thematic spread; the range is infinite. We encourage the use of all media!
-    `,
+    description: `It is often difficult to read through dense blocks of text without something to rest your eyes on. In the Art Department, we strive to embellish the newspaper with our unique illustrations, each perfectly suited for its article. In a school where artistic abilities are not seen as a priority, you can easily find your niche as an artist for The Spectator, where your art will be displayed for all of its readers to admire. The limits of your creativity have no bounds; you can explore and develop your art skills by taking on cool and unique requests, from a portrait of Taylor Swift (A&E) to an oily stork (Humor), as well as the occasional thematic spread; the range is infinite. We encourage the use of all media!`,
     quotes: [],
     link:
       "",
@@ -107,7 +101,7 @@ export const SECTIONS = [
   {
     name: "Science",
     imageLink: null,
-    description: `The Science Department covers everything science-related, from pandemics and groundbreaking discoveries to Stuyvesant’s very own fascinating science clubs and events. Our team captures the complexity of science through analytical and often opinion-based writing, and we aim to convey the intricacies of the world around us and beyond to the student body.`,
+    description: `The Science Department covers everything science-related, from pandemics and groundbreaking discoveries to Stuyvesant’s very own fascinating science clubs and events. Our department captures the complexity of science through analytical and often opinion-based writing, and we aim to convey the intricacies of the world around us and beyond to the student body.`,
     quotes: [],
     link:
       "",
