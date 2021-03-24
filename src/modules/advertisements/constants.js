bd9126839228e55561b0393223c5baadde765712
Andrey Sokolov <andrey.sokolov123@gmail.com>
2021-03-24 01:48:32 -0400

    whoopsie

--- src/modules/advertisements/constants.js
+++ src/modules/advertisements/constants.js
@@ -7,5 +7,5 @@ export const pathToAds = process.env.PUBLIC_URL + "/img/ads/";
 
 export const adRedirects = [['kweller', "http://www.kwellerprep.com"],
                             ['aimplus', "http://www.aimpluseducation.com"],
-                            ['kweller', "https://thetutorverse.com/stuy"]
+                            ['tutorverse', "https://thetutorverse.com/stuy"]
                              ];
