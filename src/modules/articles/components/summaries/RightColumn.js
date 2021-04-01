712f438f9c8f08a65ecfd94276adc66f471cc095
Andrey Sokolov <andrey.sokolov123@gmail.com>
2021-03-31 23:56:38 -0400

    mediacolumnupdate

--- src/modules/articles/components/summaries/RightColumn.js
+++ src/modules/articles/components/summaries/RightColumn.js
@@ -112,9 +112,8 @@ const RightColumn = ({ classes, articles }) => {
       <Col xsHidden sm={3} md={3} lg={3} className={classes.RightColumn} />
     );
   }
-  const [primaryArticle, secondaryArticle] = articles;
+  const [primaryArticle] = articles;
   const primarySection = primaryArticle.section;
-  const secondarySection = secondaryArticle.section;
 
   return (
     <Col xsHidden sm={3} md={3} lg={3} className={classes.RightColumn}>
@@ -159,39 +158,6 @@ const RightColumn = ({ classes, articles }) => {
           <p className={classes.preview}>{primaryArticle.preview}</p>
           <Byline contributors={primaryArticle.contributors} />
           <Dateline timestamp={primaryArticle.created_at} />
-        </div>
-      )}
-
-      {secondaryArticle && (
-        <div className={classes.secondaryArticle}>
-          {secondaryArticle.media.length > 0 && (
-            <div>
-              <Link
-                to={`${secondarySection.permalink}/${secondaryArticle.slug}`}
-              >
-                <figure className={classes.figure}>
-                  <img src={secondaryArticle.media[0].attachment_url} 
-                       alt={secondaryArticle.media[0].title}
-                  />
-                </figure>
-              </Link>
-            </div>
-          )}
-          <Link
-            to={secondarySection.permalink}
-            className={classes.sectionLabel}
-          >
-            {secondarySection.name}
-          </Link>
-          <Link
-            to={`${secondarySection.permalink}/${secondaryArticle.slug}`}
-            className={classes.articleTitle}
-          >
-            {secondaryArticle.title}
-          </Link>
-          <p className={classes.preview}>{secondaryArticle.preview}</p>
-          <Byline contributors={secondaryArticle.contributors} />
-          <Dateline timestamp={secondaryArticle.created_at} />
         </div>
       )}
     </Col>
