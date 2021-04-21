b500f8ea69f0750d96975e7e01ff9e43cd80bd09
dchen278 <reindeer914@gmail.com>
2021-04-21 12:58:33 -0400

    Instagram Icon and exports

--- src/modules/core/icons/index.js
+++ src/modules/core/icons/index.js
@@ -2,3 +2,5 @@ export { default as Hamburger } from "./Hamburger";
 export { default as Search } from "./Search";
 export { default as Print } from "./Print";
 export { default as LoadingIcon } from "./LoadingIcon";
+export { default as Instagram } from './Instagram'
+export { default as Facebook } from './Facebook'
