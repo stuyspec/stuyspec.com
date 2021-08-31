6c548c54b09739cfd1787eb12d76f232b0c8c3b0
Andrey Sokolov <andrey.sokolov123@gmail.com>
2021-08-31 13:44:21 -0400

    fix the duplicate text on the mobile version

--- src/modules/core/components/PageFooter.tsx
+++ src/modules/core/components/PageFooter.tsx
@@ -151,11 +151,11 @@ const styles = {
     sectionFlex: {
       flexWrap: "nowrap",
       height: "auto",
-      "& > div:nth-child(10) ~ div": {
-        // shows first ten section blocks
+      "& > div:nth-child(9) ~ div": {
+        // shows first nine section blocks
         display: "none"
       },
-      "& > div:nth-child(11)": {
+      "& > div:nth-child(9)": {
         borderBottom: "1px solid #ddd"
       }
     },
