5447c0012c5f0664953587d81b6d41624ab36fe3
David Chen <76599476+dchen278@users.noreply.github.com>
2022-08-11 14:38:11 -0400

    remove manhattanace ad

--- src/modules/advertisements/constants.js
+++ src/modules/advertisements/constants.js
@@ -1,15 +1,15 @@
 export default {
-  NAME: 'advertisements',
+  NAME: "advertisements",
 };
 
 // PUBLIC_URL replaced by path to public folder at build time
 export const pathToAds = `${process.env.PUBLIC_URL}/img/ads/`;
 
-export const adRedirects = [['kweller', 'http://www.kwellerprep.com'],
-  ['manhattanace', 'http://www.manhattan-ace.org/'],
-  ['georgetown', 'https://qrco.de/stuy-hoya'],
-  ['parsons', 'https://qrco.de/stuy-parsons'],
-  ['columbia', 'https://qrco.de/stuy-cbs'],
-  ['rochester', 'https://qrco.de/stuy-roch'],
-  ['dontsweattheessay', 'https://www.dontsweattheessay.com']
+export const adRedirects = [
+  ["kweller", "http://www.kwellerprep.com"],
+  ["georgetown", "https://qrco.de/stuy-hoya"],
+  ["parsons", "https://qrco.de/stuy-parsons"],
+  ["columbia", "https://qrco.de/stuy-cbs"],
+  ["rochester", "https://qrco.de/stuy-roch"],
+  ["dontsweattheessay", "https://www.dontsweattheessay.com"],
 ];
--- src/modules/advertisements/reducer.js
+++ src/modules/advertisements/reducer.js
@@ -1,39 +1,34 @@
 const initialState = [
   {
-    company: 'Kweller Prep',
-    filename: 'kweller.png',
-    url: '/ad/kweller',
+    company: "Kweller Prep",
+    filename: "kweller.png",
+    url: "/ad/kweller",
   },
   {
-    company: 'Manhattan Ace',
-    filename: 'manhattanace.png',
-    url: '/ad/manhattanace',
+    company: "Georgetown University",
+    filename: "Georgetown.png",
+    url: "/ad/georgetown",
   },
   {
-    company: 'Georgetown University',
-    filename: 'Georgetown.png',
-    url: '/ad/georgetown',
+    company: "Parsons Paris",
+    filename: "Parsons.png",
+    url: "/ad/parsons",
   },
   {
-    company: 'Parsons Paris',
-    filename: 'Parsons.png',
-    url: '/ad/parsons',
+    company: "Columbia University",
+    filename: "Columbia.png",
+    url: "/ad/columbia",
   },
   {
-    company: 'Columbia University',
-    filename: 'Columbia.png',
-    url: '/ad/columbia',
+    company: "University of Rochester",
+    filename: "Rochester.png",
+    url: "/ad/rochester",
   },
   {
-    company: 'University of Rochester',
-    filename: 'Rochester.png',
-    url: '/ad/rochester',
+    company: "Don't Sweat the Essay",
+    filename: "DontSweatTheEssay.png",
+    url: "/ad/dontsweattheessay",
   },
-  {
-    company: 'Don\'t Sweat the Essay',
-    filename: 'DontSweatTheEssay.png',
-    url: '/ad/dontsweattheessay',
-  }
 ];
 
 const reducer = (state = { ...initialState }) => state;
