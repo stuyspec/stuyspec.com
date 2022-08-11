fcc1204635732ef2ca947620a213edcc8450e585
David Chen <76599476+dchen278@users.noreply.github.com>
2022-08-11 15:27:12 -0400

    update package lock and delete jest config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  rootDir: "src/tests",
-  testRegex: "/src/tests/.*test\\.tsx$",
-  setupFiles: ["<rootDir>/App.spec.js"],
-  moduleNameMapper: {
-    "^.+\\.(css|scss)$": "<rootDir>/__mocks__/CSSStub.config.js",
-    "react-dotenv": "<rootDir>/__mocks__/react-dotenv.tsx",
-  },
-};
