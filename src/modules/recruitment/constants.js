b4a91de81d03187fc666bf44ed525117ebb5ab57
David Chen <76599476+dchen278@users.noreply.github.com>
2022-10-08 21:08:33 -0400

    add department applications

--- src/modules/recruitment/constants.js
+++ src/modules/recruitment/constants.js
@@ -1,33 +1,36 @@
 export const SECTIONS = [
   {
-    name: 'Opinions',
+    name: "Opinions",
     imageLink: null,
-    description: 'The Opinions section lets writers share their views on issues relevant within the country, Stuyvesant, and their personal lives. Writers across all grades give their opinions on subjects ranging from politics and economics to technology and culture and share their personal stories. We try to consistently create engaging, substantive, and relevant content to provide students with a closer look at the world and the Stuyvesant community.',
+    description:
+      "The Opinions section lets writers share their views on issues relevant within the country, Stuyvesant, and their personal lives. Writers across all grades give their opinions on subjects ranging from politics and economics to technology and culture and share their personal stories. We try to consistently create engaging, substantive, and relevant content to provide students with a closer look at the world and the Stuyvesant community.",
     quotes: [],
-    deadline: '',
+    deadline: "Application due November 29th, 2022",
     link:
-      '',
+      "https://docs.google.com/document/d/1iTVCDjz5qnw22GEEhImaNNSmxF6v7q91aHhjeJ1Elqc/edit",
   },
   {
-    name: 'Arts and Ent.',
+    name: "Arts and Ent.",
     imageLink: null,
-    description: 'As the only department with more than three article categories, Arts and Entertainment is notable for its versatility and creative freedom. We review recent cultural topics such as art, fashion, film, food, literature, music, and more! A&E writers are also free to experiment with the thinkpiece category, where one can explore cultural phenomena or trends, gender standards, or race from a cultural standpoint. Whether you are an aspiring music enthusiast or cultural connoisseur, everyone can find (or create!) their own niche in A&E.',
+    description:
+      "As the only department with more than three article categories, Arts and Entertainment is notable for its versatility and creative freedom. We review recent cultural topics such as art, fashion, film, food, literature, music, and more! A&E writers are also free to experiment with the thinkpiece category, where one can explore cultural phenomena or trends, gender standards, or race from a cultural standpoint. Whether you are an aspiring music enthusiast or cultural connoisseur, everyone can find (or create!) their own niche in A&E.",
     quotes: [],
-    deadline: '',
+    deadline: "Application due November 12th, 2022",
     link:
-      '',
+      "https://docs.google.com/document/d/1Hso2Wj2DfZm5NwQ5VAgqgM9jbBOxjqpYgREUH6qgdTY/edit",
   },
   {
-    name: 'Features',
+    name: "Features",
     imageLink: null,
-    description: 'Features capture the spirit and mood of the school. At times, Features covers Stuyvesant’s evergreen topics, e.g. what is it about PE uniforms that gets everybody so hot and bothered? Other times, writers might profile a teacher. Other times, writers share creative narratives about experiences they have had in the “Voices” section. There is much to be found in Features, so come hither!',
+    description:
+      "Features capture the spirit and mood of the school. At times, Features covers Stuyvesant’s evergreen topics, e.g. what is it about PE uniforms that gets everybody so hot and bothered? Other times, writers might profile a teacher. Other times, writers share creative narratives about experiences they have had in the “Voices” section. There is much to be found in Features, so come hither!",
     quotes: [],
-    deadline: '',
+    deadline: "Application due October 22, 2022",
     link:
-      '',
+      "https://docs.google.com/document/d/1mkO6ErHiwlRH5qWKcB-jPeTr2hp23hNuEKR-hwn0wno/edit?usp=sharing",
   },
   {
-    name: 'Humor',
+    name: "Humor",
     imageLink: null,
     description: `Do you want to lowkey rant about your life and roast everything?
         Would you like to have a personality glow up?
@@ -35,88 +38,97 @@ export const SECTIONS = [
         Join Spec Humor!
         We write funny and satirical pieces for The Spec and unlike other departments, everything we publish is libel and slander which means that you'll be in control of what everyone says!`,
     quotes: [],
-    deadline: '',
-    link: '',
+    deadline: "Application due",
+    link:
+      "https://docs.google.com/forms/d/e/1FAIpQLSfR7V1h76tdkPAcCiXQ915knCYuHTPWnLMvalEEBjKRpaA5yw/viewform",
   },
   {
-    name: 'Sports',
+    name: "Sports",
     imageLink: null,
-    description: 'The Sports department covers all sports topics, whether it be PSAL sports at Stuyvesant or professional sports. Articles pertaining to Stuyvesant sports range from championship articles and season updates to athlete and coach interviews and opinions. Writers also cover virtually any topic in the larger sports world, from debates on Messi vs Ronaldo and college basketball to NFL playoffs and the Champions League. If that’s not enough to convince you to apply, we also have amazing commentary, livestreaming, and social media projects that you could be a part of!',
+    description:
+      "The Sports department covers all sports topics, whether it be PSAL sports at Stuyvesant or professional sports. Articles pertaining to Stuyvesant sports range from championship articles and season updates to athlete and coach interviews and opinions. Writers also cover virtually any topic in the larger sports world, from debates on Messi vs Ronaldo and college basketball to NFL playoffs and the Champions League. If that’s not enough to convince you to apply, we also have amazing commentary, livestreaming, and social media projects that you could be a part of!",
     quotes: [],
-    deadline: '',
+    deadline: "Application due November 1st, 2022",
     link:
-      '',
+      "https://docs.google.com/document/d/15lvrc9EC5mTG6WPTWC1jwggiGM7iZ83xNUe8vLC9qaU/edit",
   },
   {
-    name: 'News',
+    name: "News",
     imageLink: null,
-    description: 'If you want your work to be featured on the front page, join News! Our department covers important school events, administrative changes, student organizations, school and student government policies, and out-of-school news. We also cover investigative pieces on controversial topics, such as the trend of uncontested elections. Writers also explore different ways to convey news through spreads, graphs, and surveys. If you’re interested in learning about exclusive Stuyvesant news before everyone else, informing the student body, interviewing students, teachers, administrators, alumni, and making connections while also improving your writing skills, APPLY TO NEWS!',
+    description:
+      "If you want your work to be featured on the front page, join News! Our department covers important school events, administrative changes, student organizations, school and student government policies, and out-of-school news. We also cover investigative pieces on controversial topics, such as the trend of uncontested elections. Writers also explore different ways to convey news through spreads, graphs, and surveys. If you’re interested in learning about exclusive Stuyvesant news before everyone else, informing the student body, interviewing students, teachers, administrators, alumni, and making connections while also improving your writing skills, APPLY TO NEWS!",
     quotes: [],
-    deadline: '',
+    deadline: "Application due November 4th, 2022",
     link:
-      '',
+      "https://docs.google.com/forms/d/e/1FAIpQLSeA77OEPjAFpdxWsTBN8G6n91KaTn_2DUEuOouHuBjeGbSODw/viewform",
   },
   {
-    name: 'Copy',
+    name: "Copy",
     imageLink: null,
-    description: 'Grammar, editing, and fact-checking. Are you interested in any of those? If so, you should join the Copy Department! Members gain early access to articles and sharpen their grammar skills while editing and fact-checking departments of their choice. We are committed to catching grammatical and factual mistakes in articles with attention to detail, and we look for members who want to contribute to maintaining The Spectator’s high-quality content.',
+    description:
+      "Grammar, editing, and fact-checking. Are you interested in any of those? If so, you should join the Copy Department! Members gain early access to articles and sharpen their grammar skills while editing and fact-checking departments of their choice. We are committed to catching grammatical and factual mistakes in articles with attention to detail, and we look for members who want to contribute to maintaining The Spectator’s high-quality content.",
     quotes: [],
-    deadline: '',
+    deadline: "Application due October 21, 2022",
     link:
-      '',
+      "https://docs.google.com/forms/d/e/1FAIpQLSd8WY0iMhonzN0OGG6D3xfgEk5nTCv6FNB4ZOWJv0hLUnQ4fQ/viewform",
   },
   {
-    name: 'Web',
+    name: "Web",
     imageLink: null,
-    description: 'The Web department is responsible for designing and updating The Spectator\'s website, www.stuyspec.com. It also attempts to facilitate the learning of developing and maintaining a web application based on Ruby on Rails and React. These real-world skills are a great resume builder and provide invaluable experience in working together as a team. This is the perfect department for both computer programming enthusiasts and novices who want to help The Spectator continue to storm the frontier of online journalism. Join the BEST department!',
+    description:
+      "The Web department is responsible for designing and updating The Spectator's website, www.stuyspec.com. It also attempts to facilitate the learning of developing and maintaining a web application based on Ruby on Rails and React. These real-world skills are a great resume builder and provide invaluable experience in working together as a team. This is the perfect department for both computer programming enthusiasts and novices who want to help The Spectator continue to storm the frontier of online journalism. Join the BEST department!",
     quotes: [],
-    deadline: '',
-    link:
-      '',
+    deadline: "Application due",
+    link: "",
   },
   {
-    name: 'Layout',
+    name: "Layout",
     imageLink: null,
-    description: 'A good publication should interest readers before they even pick it up and read it; the Layout department does just that! We are responsible for formatting all of the articles, photos, art, and other media and make sure that the paper is aesthetically appealing and easy to read. After all, no one wants to read ugly and confusing blocks of text. With creativity and a certain magic called InDesign, we present to you what you know as The Spectator. As a staff member, you will be taught how to use InDesign and asked to critique the formatting of each issue as it is released.',
+    description:
+      "A good publication should interest readers before they even pick it up and read it; the Layout department does just that! We are responsible for formatting all of the articles, photos, art, and other media and make sure that the paper is aesthetically appealing and easy to read. After all, no one wants to read ugly and confusing blocks of text. With creativity and a certain magic called InDesign, we present to you what you know as The Spectator. As a staff member, you will be taught how to use InDesign and asked to critique the formatting of each issue as it is released.",
     quotes: [],
-    deadline: '',
+    deadline: "Application due October 23, 2022",
     link:
-      '',
+      "https://docs.google.com/forms/d/e/1FAIpQLScw5AKD7_5yDm2DZ6gkOP4AQObaDz63lxaz8sfoEv0gvXm8pA/viewform",
   },
   {
-    name: 'Photo',
+    name: "Photo",
     imageLink: null,
-    description: 'Photo is dedicated to providing interesting and dynamic photographs to support the articles in The Spectator and spearhead photo essay projects. Typical assignments include portraits of students and teachers, photos at school events, and shots of fun things around the city. This department is a good fit for anyone interested in photography, whether you’re just starting out, or you’re an expert looking for a place to practice and refine your skills.',
+    description:
+      "Photo is dedicated to providing interesting and dynamic photographs to support the articles in The Spectator and spearhead photo essay projects. Typical assignments include portraits of students and teachers, photos at school events, and shots of fun things around the city. This department is a good fit for anyone interested in photography, whether you’re just starting out, or you’re an expert looking for a place to practice and refine your skills.",
     quotes: [],
-    deadline: '',
+    deadline: "Application due October 28, 2022",
     link:
-      '',
+      "https://docs.google.com/forms/d/e/1FAIpQLSfWi0Lk3QVdhfIBcyFFaRrTPWhu39COJ3aSpkRKyCVJS01ETA/viewform",
   },
   {
-    name: 'Business',
+    name: "Business",
     imageLink: null,
-    description: 'The Business department is the backbone of The Spectator. Due to the efforts of this department, The Spectator is able to print all of its issues while still remaining independent. We negotiate and secure deals with businesses all over New York as well as handle the advertisements on the website and in the paper. Members learn skills that can be applied to real life, including talking in formal settings, learning how to negotiate and compromise, and understanding how to manage money.',
+    description:
+      "The Business department is the backbone of The Spectator. Due to the efforts of this department, The Spectator is able to print all of its issues while still remaining independent. We negotiate and secure deals with businesses all over New York as well as handle the advertisements on the website and in the paper. Members learn skills that can be applied to real life, including talking in formal settings, learning how to negotiate and compromise, and understanding how to manage money.",
     quotes: [],
-    deadline: '',
+    deadline: "Application due October 23rd, 2022",
     link:
-      '',
+      "https://docs.google.com/forms/d/e/1FAIpQLSf_6SAN2EHv_t9GwbzS46yDO5FrW1edzpfrmM5IRvWix_K9Eg/viewform",
   },
   {
-    name: 'Art',
+    name: "Art",
     imageLink: null,
-    description: 'It is often difficult to read through dense blocks of text without something to rest your eyes on. In the Art Department, we strive to embellish the newspaper with our unique illustrations, each perfectly suited for its article. In a school where artistic abilities are not seen as a priority, you can easily find your niche as an artist for The Spectator, where your art will be displayed for all of its readers to admire. The limits of your creativity have no bounds; you can explore and develop your art skills by taking on cool and unique requests, from a portrait of Taylor Swift (A&E) to an oily stork (Humor), as well as the occasional thematic spread; the range is infinite. We encourage the use of all media!',
+    description:
+      "It is often difficult to read through dense blocks of text without something to rest your eyes on. In the Art Department, we strive to embellish the newspaper with our unique illustrations, each perfectly suited for its article. In a school where artistic abilities are not seen as a priority, you can easily find your niche as an artist for The Spectator, where your art will be displayed for all of its readers to admire. The limits of your creativity have no bounds; you can explore and develop your art skills by taking on cool and unique requests, from a portrait of Taylor Swift (A&E) to an oily stork (Humor), as well as the occasional thematic spread; the range is infinite. We encourage the use of all media!",
     quotes: [],
-    deadline: '',
+    deadline: "Application due October 28th, 2022",
     link:
-      '',
+      "https://docs.google.com/forms/d/e/1FAIpQLSe86KPZcmcm3WBaRuHNhUHkT0AnQhBoQyMxUj6pdzTdsMLrfw/viewform",
   },
   {
-    name: 'Science',
+    name: "Science",
     imageLink: null,
-    description: 'The Science Department covers everything science-related, from pandemics and groundbreaking discoveries to Stuyvesant’s very own fascinating science clubs and events. Our department captures the complexity of science through analytical and often opinion-based writing, and we aim to convey the intricacies of the world around us and beyond to the student body.',
+    description:
+      "The Science Department covers everything science-related, from pandemics and groundbreaking discoveries to Stuyvesant’s very own fascinating science clubs and events. Our department captures the complexity of science through analytical and often opinion-based writing, and we aim to convey the intricacies of the world around us and beyond to the student body.",
     quotes: [],
-    deadline: '',
+    deadline: "Application due November 11th, 2022",
     link:
-      '',
+      "https://docs.google.com/document/d/1PTYnVsdjnox6vGrlOaZCBmOoQUK8f8VnSeCoxbX3Leo/edit",
   },
 ];
